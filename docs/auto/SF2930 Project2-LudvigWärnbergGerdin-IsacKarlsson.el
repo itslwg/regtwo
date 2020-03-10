eef5cbc8cb98d5fde762d985092787a9f58acde7
Ludde <ludviggerdin@gmail.com>
2020-03-10 13:57:46 +0100

    #	modified:   "docs/SF2930 Project2-LudvigW\303\244rnbergGerdin-IsacKarlsson.org"

--- docs/auto/SF2930 Project2-LudvigWärnbergGerdin-IsacKarlsson.el
+++ docs/auto/SF2930 Project2-LudvigWärnbergGerdin-IsacKarlsson.el
@@ -6,15 +6,17 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "margin=1.25in")))
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
+    "../performance_table"
+    "../risk_groups"
     "article"
     "art11"
     "inputenc"
@@ -37,17 +39,19 @@
     "amsthm"
     "bookmark")
    (LaTeX-add-labels
-    "sec:org98daf89"
-    "sec:orga14df41"
-    "sec:org92878bd"
-    "sec:org0d20bb8"
-    "sec:orgff1afec"
-    "sec:org2434c16"
-    "sec:orge528f3b"
-    "sec:org6ae7275"
-    "sec:org1383086"
-    "sec:org898dae6"
-    "sec:org13e31d1")
+    "sec:org5b81261"
+    "sec:org66de239"
+    "sec:org821dc54"
+    "sec:org408fb19"
+    "sec:orgd4d9168"
+    "sec:org00ad7b6"
+    "sec:org12395d3"
+    "sec:orgc31a25a"
+    "sec:org82717ed"
+    "sec:org73a0f29"
+    "sec:org1c88ca1"
+    "sec:orga0f5b00"
+    "sec:orgc79e91f")
    (LaTeX-add-amsthm-newtheorems
     "definition"))
  :latex)
